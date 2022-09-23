e41606a31b0ef9d0e0dbf134a80c7f01341f783d
Micah Bottoms <bottoms.micah.3@gmail.com>
2022-09-23 09:14:01 -0600

    refactored script.js and edited index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,19 @@
-var searchBtn = document.getElementById('searchBtn');
-var city = document.getElementById('search-weather');
-var results = document.querySelector('.results');
-function getWeather(userInput) {
-    var myKey = "b5f2c6f61d508cd8a83c094b4381aa02";   
-    var input = userInput;
-    var weather = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${myKey}`;
-    fetch(weather)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function(data){
-        var weatherData = [
-            `Temperature: ${data.current.temp}`,
-            `Humidity: ${data.current.humidity}`,
-            `Wind: ${data.current.wind_speed}`,
-            `UV Index: ${data.current.uvi}`
-        ];
-    for (var i = 0; i < weatherData.length; i++) {
-        weatherData.eq(i).text(weatherData[i]);  
-    }
-    
-    })
-    .catch(function (e) {
-        console.log(e);
-      });
-} 
+const ft = new Fetch();
+const ui = new UI();
 
+const search = document.getElementById("searchUser");
+const button = document.getElementById("submit");
+button.addEventListener("click", () => {
+  const currentVal = search.value;
 
-
-searchBtn.on("click", () => {
-    var searchInput = city.val().trim();
-    // console.log(input);
-    getWeather(searchInput);
-    city.val("");
+  ft.getCurrent(currentVal).then((data) => {
+    ui.populateUI(data);
+    ui.saveToLS(data);
   });
+});
+
+
+window.addEventListener("DOMContentLoaded", () => {
+  const dataSaved = ui.getFromLS();
+  ui.populateUI(dataSaved);
+});
