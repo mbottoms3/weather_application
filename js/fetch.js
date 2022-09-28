ed8bd1c793fbdb037a22d91d110b1b160f61b9eb
Micah Bottoms <bottoms.micah.3@gmail.com>
2022-09-28 11:30:36 -0600

    debugged fetch request

--- js/fetch.js
+++ js/fetch.js
@@ -4,7 +4,7 @@ class FetchClass {
       const myKey = "b5f2c6f61d508cd8a83c094b4381aa02";
       
       // Fetches city lat and lon for the city the user inputs
-      const geo = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=1&appid=${myKey}`);
+      const geo = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=1&appid=${myKey}`);
 
       // Stores return in order to access lat and lon coordinates
       const coordinates = await geo.json();
@@ -12,7 +12,7 @@ class FetchClass {
 
       // Fetches 5 day forecast using city coordinates and returns data
       const forecast = await fetch(
-        `http://api.openweathermap.org/data/2.5/forecast?lat=${coordinates[0].lat}&lon=${coordinates[0].lon}&appid=${myKey}&units=imperial`
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates[0].lat}&lon=${coordinates[0].lon}&appid=${myKey}&units=imperial`
       );
       
       // Stores data in const
